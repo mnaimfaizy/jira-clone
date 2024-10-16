8d3235e67aaa44758fd56c6aa01c93d624455519
Mohammad Naim Faizy <mnaim.faizy@gmail.com>
2024-10-17 10:29:39 +1100

    chore: building a session meddileware and logout functionality

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,28 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
+import { useLogout } from "@/features/auth/api/use-logout";
+import { useCurrent } from "@/features/auth/api/user-current";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 
 export default function Home() {
+  const router = useRouter();
+
+  const { data, isLoading } = useCurrent();
+
+  const { mutate } = useLogout();
+
+  useEffect(() => {
+    if(!data && !isLoading) {
+      router.push("/sign-in") 
+    }
+  }, [data, isLoading, router])
+
   return (
     <div>
-      Hello World!
-      <Button>Click me</Button>
-      <Button variant="destructive">Destructive</Button>
-      <Button variant="secondary">Secondary</Button>
-      <Button variant="ghost">Ghost</Button>
-      <Button variant="muted">Muted</Button>
-      <Button variant="outline">Outline</Button>
-      <Button variant="tertiary">Tertiary</Button>
-
-      <Input />
-      <p className="text-red-500 text-semibold">This is a red text</p>
+      Only visible to authenticated users
+      <Button onClick={() => mutate()}>Logout</Button>
     </div>
   );
 }
