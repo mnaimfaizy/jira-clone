d450c86f3e235ea51d24c88b8f5bd43f1076ffac
Mohammad Naim Faizy <mnaim.faizy@gmail.com>
2024-10-19 11:41:08 +1100

    chore: building the workspace form

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { Toaster } from "@/components/ui/sonner";
 
 import { cn } from "@/lib/utils";
 import QueryProvider from "@/components/query-provider";
@@ -23,6 +24,7 @@ export default function RootLayout({
         className={cn(inter.className, "antialiased min-h-screen")}
       >
         <QueryProvider>
+          <Toaster />
           {children}
         </QueryProvider>
       </body>
--- src/features/auth/api/use-register.ts
+++ src/features/auth/api/use-register.ts
@@ -3,6 +3,7 @@ import { InferRequestType, InferResponseType } from "hono";
 
 import { client } from "@/lib/rpc";
 import { useRouter } from "next/navigation";
+import { toast } from "sonner";
 
 type ResponseType = InferResponseType<typeof client.api.auth.register["$post"]>;
 type RequestType = InferRequestType<typeof client.api.auth.register["$post"]>;
@@ -14,11 +15,21 @@ export const useRegister = () => {
     const mutation = useMutation<ResponseType, Error, RequestType>({
         mutationFn: async ({json}) => {
             const response = await client.api.auth.register["$post"]({ json });
+
+            if(!response.ok) {
+                throw new Error("Failed to register");
+            }
+
             return await response.json();
         },
         onSuccess: () => {
+            toast.success("Registration successful");
             router.refresh();
             queryClient.invalidateQueries({ queryKey: ["current"] });
+        },
+        onError: (error) => {
+            console.error(error);
+            toast.error("Failed to register");
         }
     })
 
