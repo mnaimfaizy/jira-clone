34a456104d6dc8d4c741dd56480e98d2589ad19d
Mohammad Naim Faizy <mnaim.faizy@gmail.com>
2024-10-31 05:51:46 +1100

    chore: Building a standalone layout

--- src/features/workspaces/components/create-workspace-form.tsx
+++ src/features/workspaces/components/create-workspace-form.tsx
@@ -16,6 +16,7 @@ import Image from "next/image";
 import { AvatarFallback } from "@radix-ui/react-avatar";
 import { ImageIcon } from "lucide-react";
 import { useRouter } from "next/navigation";
+import { cn } from "@/lib/utils";
 
 interface CreateWorkspaceFormProps {
     onCancel?: () => void;
@@ -146,6 +147,7 @@ export const CreateWorkspaceForm = ({ onCancel }: CreateWorkspaceFormProps) => {
                                 variant="secondary"
                                 onClick={onCancel}
                                 disabled={isPending}
+                                className={cn(!onCancel && "invisible")}
                             >
                                 Cancel
                             </Button>
